85df407850d8edb1e8e13bf4ba2935d22c21af61
Pedro Paiva <pedro.paiva@somagrupo.com.br>
2025-04-28 03:23:52 -0300

    main

--- src/services/index.jsx
+++ src/services/index.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const USER_SURVEYS_URL = axios.create({
-  baseURL: 'http://surveysresponsebackend-production.up.railway.app',
+  baseURL: 'https://surveysresponsebackendproduction.up.railway.app',
   // baseURL: 'http://localhost:8080',
 });
